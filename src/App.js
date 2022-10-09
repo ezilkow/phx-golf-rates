450ed4e629e9ef8497e240d2b42a5a587a650510
Evan G. Zilkow <ezilkow@gmail.copm>
2022-10-09 15:33:07 -0700

    Changed Header Name and Image, changed Greeting text

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hell World, PhoenixGolfRates coming soon!</h1>
+        <h1>Hello World, PhxGolfRates coming soon!</h1>
       </header>
     </div>
   );
